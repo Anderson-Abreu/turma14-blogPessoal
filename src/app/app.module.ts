e51999a8396cb4e261cb0d5e18b47a89e4cdba15
Anderson-Abreu <prof.andersonmabreu@gmail.com>
2021-03-02 21:34:05 -0300

    crud de postagem completo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { InicioComponent } from './inicio/inicio.component';
 import { TemaComponent } from './tema/tema.component';
 import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
 import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
+import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
+import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,9 @@ import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
     InicioComponent,
     TemaComponent,
     TemaDeleteComponent,
-    TemaEditComponent
+    TemaEditComponent,
+    PostagemDeleteComponent,
+    PostagemEditComponent
   ],
   imports: [
     BrowserModule,
